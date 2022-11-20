39334ba3d9056064328e9f8cc0794000ce291e79
Darklevelss <becerrafeli12@gmail.com>
2022-11-20 11:18:33 -0500

    ya logica

--- src/logic/Elections.java
+++ src/logic/Elections.java
@@ -6,11 +6,23 @@ import java.util.Random;
 public class Elections {
 
     private int [][] votes;
-    public Elections( int columns, int rows ) {
+    private Candidate []candidates;
+    private Town []towns;
 
+    /***
+     * constructor
+     * @param candidates arreglo de candidatos
+     * @param towns arreglo de ciudades
+     */
+    public Elections( Candidate[]candidates,Town[]towns ) {
 
-        votes=new int [rows][columns];
+        this.candidates=candidates;
+        this.towns=towns;
+        votes=new int [candidates.length][towns.length];
         fillMatriz();
+        totalOfVotesCandidate();
+        totalOfVotesTowns();
+
 
     }
    private void fillMatriz(){
@@ -23,8 +35,99 @@ public class Elections {
             }
         }
 
-    public int[][] getVotes(){
+    /***
+     * metodo que me retorna la matriz de votos
+      * @return
+     */
+    private int[][] getVotes(){
         return votes.clone();
+    }
+    //establece el total de votos de cada candidatos
+    private void totalOfVotesCandidate(){
+    for (int i =0;i<votes.length;i++){
+        for(int j=0;j<votes[0].length;j++){
+            candidates[i].setTotalVotes(candidates[i].getTotalVotes()+votes[i][j]);
+        }
+    }
+    }
+    //setea el total de votos de cada ciudad
+    private void totalOfVotesTowns(){
+        for (int i =0;i<votes.length;i++){
+            for(int j=0;j<votes[0].length;j++){
+                towns[i].setTotalVotes(towns[i].getTotalVotes()+votes[j][i]);
+            }
+        }
+
+    }
+    //retorna la posicion de el candidato ganador
+    public Candidate getWinner(){
+        int pos=0;
+        for (int i=1;i< candidates.length;i++){
+            if(candidates[i-1].getTotalVotes()<candidates[i].getTotalVotes()){
+                pos=i;
+            }
+        }
+        return candidates[pos];
+    }
+
+    /***
+     * recibe la posicion del candidato y establece cual es el promedio de votos
+     * @param pos posicion del candidato(usar con el findCandidate
+     * @return el promedio
+     */
+    public double average(int pos){
+
+        return candidates[pos].getTotalVotes()/ towns.length;
+    }
+
+    /**
+     * metodo que recibe la posicion del candidato y me retorna donde tuvo menos votos
+     * @param posCandidate posicion del candidato(usarlo con el findCandidate)
+     * @return pos dentro del arreglo de ciudades donde obtuvo menos votos
+     */
+    public int getLessVotes(int posCandidate){
+        int posLess=0,min=0;
+        for(int i=0;i<votes.length;i++){
+            if(votes[posCandidate][i]<min){
+                posLess=i;
+                min=votes[posCandidate][i];
+            }
+        }
+        return posLess;
+    }
+
+    /***
+     * método que me retorna donde un candidato tuvo mas votos
+     * @param posCandidate posicion del candidato(usarlo con el findCandidate)
+     * @return pos dentro del arreglo de ciudades donde obtuvo más votos
+     */
+    public int getMostVotes(int posCandidate){
+        int posMost=0, max=0;
+
+        for(int i=0;i<votes.length;i++){
+            if(votes[posCandidate][i]>max){
+                posMost=i;
+                max=votes[posCandidate][i];
+            }
+        }
+        return posMost;
+    }
+
+    /***
+     * metodo privado que me encuentra a un candidato por el nombre, si pos=-1 lanzar un error
+     * @param name nombre del candidato
+     * @return posicion del candidato
+     */
+    public int findCandidate(String name){
+        int pos=-1;
+
+        for (int i=0;i<candidates.length;i++){
+            if(candidates[i].getName().equalsIgnoreCase(name)){
+                pos=i;
+            }
+        }
+
+        return pos;
     }
 
 }
--- src/logic/Town.java
+++ src/logic/Town.java
@@ -4,7 +4,7 @@ package logic;
 
 public class Town {
     private String name;
-
+    private int totalVotes;
 
     public Town(String name) {
         this.name = name;
@@ -12,5 +12,17 @@ public class Town {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(int totalOfVotes) {
+        this.totalVotes = totalOfVotes;
     }
 }
